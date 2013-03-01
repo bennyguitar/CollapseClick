3e4272fe03d3df9119eb1ec12449c60ac9cae3fc
Benjamin Gordon <brgordon@ua.edu>
2013-02-28 19:15:45 -0600

    Initial Commit

---
+++ CollapseClick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollapseClick
+//
+//  Created by Ben Gordon on 2/28/13.
+//  Copyright (c) 2013 Ben Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CollapseClick/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CollapseClick
+//
+//  Created by Ben Gordon on 2/28/13.
+//  Copyright (c) 2013 Ben Gordon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
